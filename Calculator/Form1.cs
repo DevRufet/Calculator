c33795e26015721422803daf3c87b571858175f1
DevRufet <nesibovrufet47@gmail.com>
2023-09-28 00:17:58 +0400

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a=Convert.ToDouble(textBox1.Text);
+            double b= Convert.ToDouble(textBox2.Text);
+            string c=(a+b).ToString();
+            MessageBox.Show(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            string c = (a - b).ToString();
+            MessageBox.Show(c);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            string c = (a * b).ToString();
+            MessageBox.Show(c);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            string c = (a / b).ToString();
+            MessageBox.Show(c);
+        }
+    }
+}
